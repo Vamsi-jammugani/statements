466653caa3b4bf480992fe040c8e0853e6593bd9
Vamsi-jammugani <112849928+Vamsi-jammugani@users.noreply.github.com>
2022-09-05 13:20:57 +0530

    Add files via upload

---
+++ vamsi statement.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+int main(){
+
+int a=6,b=4;{
+if(a>b){
+	std:: cout<<"here a is greater="<<a;
+}
+ else if(a<b)
+{
+	std:: cout<<"here b is gerater="<<b;
+}else
+{
+	std:: cout<<"here both are equal";
+}}
+return 0;
+}
